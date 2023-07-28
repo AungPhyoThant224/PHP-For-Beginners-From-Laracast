9f83e3260a87bf38a84d9d0e9f92996631beb789
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-28 11:48:34 +0630

    Make a PHP Router.

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-
-$heading = "Home";
-
-require "views/index.view.php";
-
-
+require 'functions.php';
+require 'router.php';
---
+++ router.php
@@ -0,0 +1,25 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/ourMission' => 'controllers/ourMission.php',
+];
+function routeToController($uri, $routes){
+    if(array_key_exists($uri, $routes)){
+        require $routes[$uri];
+    }else{
+        abort();
+    }
+}
+
+function abort(){
+    http_response_code(404);
+    require 'views/404.php';
+    die();
+}
+
+routeToController($uri, $routes);
