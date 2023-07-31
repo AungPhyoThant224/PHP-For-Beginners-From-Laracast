4db88456b9616a86cab6acf77fdf2aaf3622fae1
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-31 10:36:01 +0630

    Rendering the Notes and Note Page.

---
+++ controllers/notes.php
@@ -0,0 +1,10 @@
+<?php
+
+$heading = "Notes";
+
+$config = require ('config.php');
+$db = new Database($config);
+
+$notes = $db->query("select * from notes where userId = 1", [])->fetchAll();
+
+require 'views/notes.view.php';
