1a21f85debaab84c316ff1b87bb015b1dfd485ea
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-31 16:25:28 +0630

    Intro to Form Validation.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,23 @@ $config = require ('config.php');
 $db = new Database($config);
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query("INSERT INTO notes(body, userId) VALUES(:body, :userId)", [
-       "body" => $_POST['body'],
-        "userId" => 1,
-    ]);
+
+    $errors = [];
+
+    if(strlen($_POST['body']) === 0){
+        $errors['body'] = 'A body is required';
+    }
+
+    if(strlen($_POST['body']) > 1000){
+        $errors['body'] = 'The body cannot be more than 1,000 character';
+    }
+
+    if(empty($errors)){
+        $db->query("INSERT INTO notes(body, userId) VALUES(:body, :userId)", [
+            "body" => $_POST['body'],
+            "userId" => 1,
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
